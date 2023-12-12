c4d3250c7ed7609f4e853c9457435d24a9493d83
vishalpukale <vishalpukale12@.com>
2023-12-12 10:42:41 +0530

    cycle or not

---
+++ 141 Linked List Cycle/script.ts
@@ -0,0 +1,22 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+       this.next = (next===undefined ? null : next)
+  }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+
+    let slow: any = head;
+    let fast: any = head;
+
+    while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast) return true;
+    }
+    return false;
+}
